83b5d60e9fafee6ee4c8d8d250e2a1596602c3c0
Catherine Parrish <cathp27@gmail.com>
2024-04-03 08:52:04 +0000

    Add account menu

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+function menu() {
+    document.getElementById("list").classList.toggle("show");
+}
+
+function profile_menu() {
+    document.getElementById("account_menu").classList.toggle("show_menu");
+}
+

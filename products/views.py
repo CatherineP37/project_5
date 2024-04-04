07b9ec7ad4dc6e6907090797d1b8eff691d146b1
Catherine Parrish <cathp27@gmail.com>
2024-04-04 13:47:06 +0000

    Edit links in index.html

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def full_collection(request):
+    """ A view to show full collection, including sorting and search queries """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/full_collection.html', context)

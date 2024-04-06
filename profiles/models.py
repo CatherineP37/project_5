a84bb9525a43b94abf156048949447e716c3eb14
Catherine Parrish <cathp27@gmail.com>
2024-04-06 15:04:33 +0000

    Edit profile.html

--- profiles/models.py
+++ profiles/models.py
@@ -15,7 +15,7 @@ class UserProfile(models.Model):
     default_phone_number = models.CharField(max_length=20,
                                             null=True, blank=True)
     default_street_address = models.CharField(max_length=80,
-                                               null=True, blank=True)   
+                                               null=True, blank=True)    
     default_town_or_city = models.CharField(max_length=40,
                                             null=True, blank=True)
     default_county = models.CharField(max_length=80,

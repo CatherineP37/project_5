ad007715238bbda7a4a5de9c0d955c7a0f0bdf96
Catherine Parrish <cathp27@gmail.com>
2024-04-07 09:36:58 +0000

    Change link name in base.html

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.index, name='index'),
     path('privacy/',views.privacy, name="privacy"),
     path('about/', views.about, name="about"),
     path('contact/', views.contact, name="contact"),

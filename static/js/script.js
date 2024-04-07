3d5c4799c9e14a4cc2a2331842055b98608773eb
Catherine Parrish <cathp27@gmail.com>
2024-04-07 13:08:02 +0000

    Edit base.css

--- static/js/script.js
+++ static/js/script.js
@@ -2,7 +2,4 @@ function menu() {
     document.getElementById("list").classList.toggle("show");
 }
 
-function profile_menu() {
-    document.getElementById("account_menu").classList.toggle("show_menu");
-}
 

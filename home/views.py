6eb4c61524ff95357619a51883df7c861b089bc5
Catherine Parrish <cathp27@gmail.com>
2024-04-07 08:39:20 +0000

    Edit views.py in home app

--- home/views.py
+++ home/views.py
@@ -6,3 +6,18 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+def contact(request):
+    """ A view to return the contact page """
+
+    return render(request, 'home/contact.html')
+
+def about(request):
+    """ A view to return the about page """
+
+    return render(request, 'home/about.html')
+
+def privacy(request):
+    """ A view to return the privacy page """
+
+    return render(request, 'home/privacy.html')

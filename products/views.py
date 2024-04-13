b91141a5bfcae577c7448d374132f4a047b9cac8
Catherine Parrish <cathp27@gmail.com>
2024-04-13 13:38:53 +0000

    Edit views.py

--- products/views.py
+++ products/views.py
@@ -30,7 +30,7 @@ def product_detail(request, product_id):
 
     context = {
         'product': product,
-        'id'=product_id,
+        'id': product_id,
     }
 
     return render(request, 'products/product_detail.html', context)

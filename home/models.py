23e9386dc92db22089f6e728e73e30e64eabc21f
Catherine Parrish <cathp27@gmail.com>
2024-04-13 11:06:38 +0000

    Edit Contact model

--- home/models.py
+++ home/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class Contact(models.Model):    
     name = models.CharField(max_length=100, null=False, blank=False)
     email = models.EmailField(max_length=300, null=False, blank=False)
-    message = models.CharField(max_length=500, null=False, blank=False)
+    message = models.TextField(null=False, blank=False, default='')
     

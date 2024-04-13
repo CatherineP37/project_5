4abbb445eeecf932c9ff5c70b44ef8730c4d6561
Catherine Parrish <cathp27@gmail.com>
2024-04-13 10:06:17 +0000

    Migrate changes to model

--- home/models.py
+++ home/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Contact(models.Model):    
-    name = models.CharField(null=False, blank=False)
-    email = models.EmailField(max_length=254, null=False, blank=False)
-    message = models.CharField(null=False, blank=False)
+    name = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(max_length=300, null=False, blank=False)
+    message = models.CharField(max_length=500, null=False, blank=False)
     

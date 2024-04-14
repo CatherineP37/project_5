0dd8e3d966e50237e6af73bc116148b54d123ce5
Catherine Parrish <cathp27@gmail.com>
2024-04-14 07:06:58 +0000

    Edit product_detail view

--- products/views.py
+++ products/views.py
@@ -21,11 +21,13 @@ def product_detail(request, product_id):
     """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
+    form = Reviews()
     
 
     
     context = {
-        'product': product,        
+        'product': product,
+        'form': form        
     }
 
     return render(request, 'products/product_detail.html', context)

739a8cf508628786e55e155a275911823009cda6
Catherine Parrish <cathp27@gmail.com>
2024-04-14 07:38:28 +0000

    Add url for update_review view

--- products/urls.py
+++ products/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
     path('delete/<int:product_id>/',
          views.delete_product,
-         name='delete_product'),    
+         name='delete_product'),  
+    path('update_review/<str:pk>/', views.update_review, name="update_review"),  
 ]

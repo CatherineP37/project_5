3009dc416fff4b5a1ff38ad3de9a0867556bb3ac
Catherine Parrish <cathp27@gmail.com>
2024-04-15 10:32:21 +0000

    Edit urls.py in home app

--- home/urls.py
+++ home/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('privacy/',views.privacy, name="privacy"),
     path('about/', views.about, name="about"),
     path('contact/', views.contact, name="contact"),
-    path('events/', views.contact, name="events"),
-    path('sitemap/', views.contact, name="sitemap"),
+    path('events/', views.events, name="events"),
+    path('sitemap/', views.sitemap, name="sitemap"),
 ]

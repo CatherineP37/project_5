c7c3321fd59d8d3cb65c05ae5d28e8f872f17e1d
Catherine Parrish <cathp27@gmail.com>
2024-04-15 10:26:37 +0000

    Add url for events.html

--- home/urls.py
+++ home/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('privacy/',views.privacy, name="privacy"),
     path('about/', views.about, name="about"),
     path('contact/', views.contact, name="contact"),
+    path('events/', views.contact, name="events"),
     path('sitemap/', views.contact, name="sitemap"),
 ]

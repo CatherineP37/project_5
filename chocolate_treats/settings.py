2054fc9ed64ee5811c410890ed982051f23eeb1f
Catherine Parrish <cathp27@gmail.com>
2024-04-24 16:11:11 +0000

    Edit settings.py

--- chocolate_treats/settings.py
+++ chocolate_treats/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-catherinep37-project5-cm1ll2nsbru.ws-eu110.gitpod.io', 'https://git.heroku.com/p-5.git']
 

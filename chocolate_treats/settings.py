79e7138eb883f2ae3d6b7efdb46de0fad247fd20
Catherine Parrish <cathp27@gmail.com>
2024-04-24 17:59:53 +0000

    Test project

--- chocolate_treats/settings.py
+++ chocolate_treats/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = 1726725232273
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-catherinep37-project5-cm1ll2nsbru.ws-eu110.gitpod.io', 'p-5-bf8a9d578875.herokuapp.com']
 
--- products/views.py
+++ products/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
 from .models import Product
-from .forms import ProductForm, Review
+from .forms import ProductForm, Review, createReview
 
 
 # Create your views here.

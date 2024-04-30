d6621f136cfaf47f2fd95038f2c7fd292a4492b5
Catherine Parrish <cathp27@gmail.com>
2024-04-30 13:53:35 +0000

    Edit delete_review

--- products/views.py
+++ products/views.py
@@ -82,8 +82,9 @@ def delete_review(request, pk):
     if request.method == "POST":
         review.delete()
         return redirect('/')
-    
-    return render(request, 'products/delete_review.html')
+    context = {'item':order}
+
+    return render(request, 'products/delete_review.html', context)
 
    
 

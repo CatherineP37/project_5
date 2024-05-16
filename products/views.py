ae2c83d69d64c4a9e716f1fade3d4d6779b15761
Catherine Parrish <cathp27@gmail.com>
2024-05-16 05:41:44 +0000

    Edit delete_review view

--- products/views.py
+++ products/views.py
@@ -103,7 +103,7 @@ def delete_review(request, review_id):
         else:
             messages.add_message(request, messages.ERROR, 'You can only delete your own reviews.')
     
-    return HttpResponseRedirect(reverse('product_detail',))
+    return render(request, 'products/update_review.html', context)
 
 
    

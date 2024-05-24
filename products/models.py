7c86b584d5a880a039620c31e0e17f8040247377
Catherine Parrish <cathp27@gmail.com>
2024-05-24 20:45:43 +0000

    Remove SKU from database

--- products/models.py
+++ products/models.py
@@ -2,8 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-class Product(models.Model):  
-    sku = models.CharField(max_length=254, null=True, blank=True)
+class Product(models.Model):    
     name = models.CharField(max_length=254)
     description = models.TextField()
     ingredients = models.TextField()

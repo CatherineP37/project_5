309b4cbbf70fc40d384dd34b71b3416e5c014180
Catherine Parrish <cathp27@gmail.com>
2024-05-31 19:50:47 +0000

    Remove print statements from webhook_handler.py

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -17,12 +17,15 @@ class StripeWH_Handler:
     def _send_confirmation_email(self, order):
         """Send the user a confirmation email"""
         cust_email = order.email
+        
         subject = render_to_string(
             'checkout/confirmation_emails/confirmation_email_subject.txt',
             {'order': order})
+        
         body = render_to_string(
             'checkout/confirmation_emails/confirmation_email_body.txt',
             {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+        
 
         send_mail(
             subject,
@@ -30,6 +33,7 @@ class StripeWH_Handler:
             settings.DEFAULT_FROM_EMAIL,
             [cust_email]
         )
+        
 
     def handle_event(self, event):
         """
@@ -95,6 +99,7 @@ class StripeWH_Handler:
                 time.sleep(1)
         if order_exists:
             self._send_confirmation_email(order)
+            print(order)
             return HttpResponse(
                 content=(f'Webhook received: {event["type"]} | SUCCESS: '
                          'Verified order already in database'),

0b47bfe8587d57671930675a27adace5adf77d8b
Catherine Parrish <cathp27@gmail.com>
2024-06-01 16:31:33 +0000

    Remove white spaces in webhook_handler.py

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -76,7 +76,7 @@ class StripeWH_Handler:
                 profile.default_country = shipping_details.address.country
                 profile.default_postcode = shipping_details.address.postal_code
                 profile.default_town_or_city = shipping_details.address.city
-                profile.default_street_address = shipping_details.address               
+                profile.default_street_address = shipping_details.address            
                 profile.default_county = shipping_details.address.state
                 profile.save()
 

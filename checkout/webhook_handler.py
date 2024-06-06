06e5d0f7db20b5b7713928ccda99730b043ac714
Catherine Parrish <cathp27@gmail.com>
2024-06-06 12:40:01 +0000

    Edit webhook_handler.py

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -76,7 +76,7 @@ class StripeWH_Handler:
                 profile.default_country = shipping_details.address.country
                 profile.default_postcode = shipping_details.address.postal_code
                 profile.default_town_or_city = shipping_details.address.city
-                profile.default_street_address = shipping_details.address            
+                profile.default_street_address = shipping_details.address.line1
                 profile.default_county = shipping_details.address.state
                 profile.save()
 
@@ -91,7 +91,7 @@ class StripeWH_Handler:
                     country__iexact=shipping_details.address.country,
                     postcode__iexact=shipping_details.address.postal_code,
                     town_or_city__iexact=shipping_details.address.city,
-                    street_address__iexact=shipping_details.address,                
+                    street_address__iexact=shipping_details.address.line1,
                     county__iexact=shipping_details.address.state,
                     grand_total=grand_total,
                     original_bag=bag,
@@ -120,7 +120,7 @@ class StripeWH_Handler:
                     country=shipping_details.address.country,
                     postcode=shipping_details.address.postal_code,
                     town_or_city=shipping_details.address.city,
-                    street_address=shipping_details.address,                    
+                    street_address=shipping_details.address.line1,
                     county=shipping_details.address.state,
                     original_bag=bag,
                     stripe_pid=pid,

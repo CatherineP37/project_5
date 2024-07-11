3a969d969a4a7958b6b8d3e4007fd662fbd2a28d
Catherine Parrish <cathp27@gmail.com>
2024-07-11 15:53:25 +0000

    Add gitpod address to ALLOWED_HOSTS in settings.py

--- chocolate_treats/settings.py
+++ chocolate_treats/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 1726725232273
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-catherinep37-project5-ubkvz9jsro3.ws-eu111.gitpod.io', 'p-5-bf8a9d578875.herokuapp.com', '8000-catherinep37-project5-ubkvz9jsro3.ws-eu114.gitpod.io',]
+ALLOWED_HOSTS = ['https://8000-catherinep37-project5-woe9rk2m8wf.ws.codeinstitute-ide.net/', 'p-5-bf8a9d578875.herokuapp.com' ]
 
 # Application definition
 
